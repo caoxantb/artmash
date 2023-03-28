80f47e861d723bca323b9678f2f6854fe0862568
Xuan An Cao <ancx@appurunoMacBook-puro-2.local>
2023-03-28 17:53:21 +0700

    temp-migrate: mash

--- app/src/components/gallery/songmash/index.tsx
+++ app/src/components/gallery/songmash/index.tsx
@@ -10,112 +10,145 @@ import trackService from "~/services/track";
 import { calcEloRating } from "~/helpers/elo-agorithm";
 import ArtistLoadingIcon from '../../icon/loading'
 import { randomizeSongs } from "~/helpers/random-song";
-import mashService from '~/services/mash';
+import { getAllFilmsInOneGallery } from "~/services/film";
 
-interface ArtistSongMashStore {
+interface FilmmashStore {
   indexLeft: number;
   indexRight: number;
-  trackLeft: Track;
-  trackRight: Track;
-  artistAllSongs: Tracks | Track[];
-  isLoading: Boolean;
-}
+  filmLeft: any;
+  filmRight: any;
+  galleryFilms: any;
+  isLoading: boolean;
+} 
 
-const ArtistSongMash = component$(({ artist }: IArtist) => {
-  const store: ArtistSongMashStore = useStore(
+// interface ArtistSongMashStore {
+//   indexLeft: number;
+//   indexRight: number;
+//   trackLeft: Track;
+//   trackRight: Track;
+//   artistAllSongs: Tracks | Track[];
+//   isLoading: Boolean;
+// }
+
+const FilmMash = component$(({ gallery }: {gallery: Gallery}) => {
+  const store: FilmmashStore = useStore(
     {
       indexLeft: 0,
       indexRight: 0,
-      trackLeft: { _id: "" },
-      trackRight: { _id: "" },
-      artistAllSongs: [],
+      filmLeft: {},
+      filmRight: {},
+      galleryFilms: [],
       isLoading: true,
     },
     { recursive: true }
   );
 
-
   useClientEffect$(async () => {
-    store.artistAllSongs = await trackService.getAllTracksByArtist(
-      artist.nameRef
-    );
-    [store.indexLeft, store.indexRight] = randomizeSongs(store.artistAllSongs.length)
+    store.galleryFilms = await getAllFilmsInOneGallery(gallery._id)
+    ;[store.indexLeft, store.indexRight] = randomizeSongs(store.galleryFilms.length)
     store.isLoading = false
   });
 
   useWatch$(async ({ track }) => {
     const indexLeft = track(() => store.indexLeft);
     const indexRight = track(() => store.indexRight);
-    store.trackLeft = store.artistAllSongs.find(
-      (track) => track.index === indexLeft
-    ) || { _id: "" };
-    store.trackRight = store.artistAllSongs.find(
-      (track) => track.index === indexRight
-    ) || { _id: "" };
+    store.filmLeft = store.galleryFilms[indexLeft]
+    store.filmRight = store.galleryFilms[indexRight]
   });
 
-  const clickHandler = $(async (event: Event, winner: string) => {
-    event.stopPropagation();
-    const target = event.target as any;
-    if (target?.localName !== "iframe") {
-      store.isLoading = true;
 
-      const [leftPoints, rightPoints] = calcEloRating(
-        store.trackLeft.points || 0,
-        store.trackRight.points || 0,
-        winner
-      );
+  return <></>
+})
 
-      console.log([leftPoints, rightPoints])
+// const ArtistSongMash = component$(({ artist }: IArtist) => {
+//   const store: any = useStore(
+//     {
+//       indexLeft: 0,
+//       indexRight: 0,
+//       trackLeft: { _id: "" },
+//       trackRight: { _id: "" },
+//       artistAllSongs: [],
+//       isLoading: true,
+//     },
+//     { recursive: true }
+//   );
 
-      const trackLeft: Track = {
-        ...(await trackService.updateTrackPoints(
-          store.trackLeft._id,
-          leftPoints
-        )),
-        points: leftPoints,
-      };
-      const trackRight: Track = {
-        ...(await trackService.updateTrackPoints(
-          store.trackRight._id,
-          rightPoints
-        )),
-        points: rightPoints,
-      };
 
-      store.artistAllSongs = store.artistAllSongs.map((track) =>
-        track.index === store.indexLeft ? trackLeft : track
-      );
-      store.artistAllSongs = store.artistAllSongs.map((track) =>
-        track.index === store.indexRight ? trackRight : track
-      );
+//   useClientEffect$(async () => {
+//     store.artistAllSongs = await trackService.getAllTracksByArtist(
+//       artist.nameRef
+//     );
+//     [store.indexLeft, store.indexRight] = randomizeSongs(store.artistAllSongs.length)
+//     store.isLoading = false
+//   });
 
-      await mashService.updateMash(artist.nameRef, [store.trackLeft.index || 0, store.trackRight.index || 0], winner)
+//   return <></> })
 
-      ;[store.indexLeft, store.indexRight] = randomizeSongs(store.artistAllSongs.length)
-      store.isLoading = false
-    }
-  });
+//   useWatch$(async ({ track }) => {
+//     const indexLeft = track(() => store.indexLeft);
+//     const indexRight = track(() => store.indexRight);
+//     store.trackLeft = store.artistAllSongs[indexLeft]
+//     store.trackRight = store.artistAllSongs[indexRight]
+//   });
 
-  return (
-    <div className="artist-songmash">
-      <ArtistTrackCard
-        track={store.trackLeft}
-        clickHandler={$((e: Event) => clickHandler(e, "left"))}
-        isVisible={store.isLoading ? "hidden" : "visible"}
-      />
-      {store.isLoading ? (
-        <ArtistLoadingIcon />
-      ) : (
-        <div className="versus">vs.</div>
-      )}
-      <ArtistTrackCard
-        track={store.trackRight}
-        clickHandler={$((e: Event) => clickHandler(e, "right"))}
-        isVisible={store.isLoading ? "hidden" : "visible"}
-      />
-    </div>
-  );
-});
+//   const clickHandler = $(async (event: Event, winner: string) => {
+//     event.stopPropagation();
+//     const target = event.target as any;
+//     if (target?.localName !== "iframe") {
+//       store.isLoading = true;
 
-export default ArtistSongMash;
+//       const [leftPoints, rightPoints] = calcEloRating(
+//         store.trackLeft.points || 0,
+//         store.trackRight.points || 0,
+//         winner
+//       );
+
+//       const trackLeft: Track = {
+//         ...(await trackService.updateTrackPoints(
+//           store.trackLeft._id,
+//           leftPoints
+//         )),
+//         points: leftPoints,
+//       };
+//       const trackRight: Track = {
+//         ...(await trackService.updateTrackPoints(
+//           store.trackRight._id,
+//           rightPoints
+//         )),
+//         points: rightPoints,
+//       };
+
+//       store.artistAllSongs = store.artistAllSongs.map((track) =>
+//         track.index === store.indexLeft ? trackLeft : track
+//       );
+//       store.artistAllSongs = store.artistAllSongs.map((track) =>
+//         track.index === store.indexRight ? trackRight : track
+//       );
+
+//       ;[store.indexLeft, store.indexRight] = randomizeSongs(store.artistAllSongs.length)
+//       store.isLoading = false
+//     }
+//   });
+
+//   return (
+//     <div className="artist-songmash">
+//       <ArtistTrackCard
+//         track={store.trackLeft}
+//         clickHandler={$((e: Event) => clickHandler(e, "left"))}
+//         isVisible={store.isLoading ? "hidden" : "visible"}
+//       />
+//       {store.isLoading ? (
+//         <ArtistLoadingIcon />
+//       ) : (
+//         <div className="versus">vs.</div>
+//       )}
+//       <ArtistTrackCard
+//         track={store.trackRight}
+//         clickHandler={$((e: Event) => clickHandler(e, "right"))}
+//         isVisible={store.isLoading ? "hidden" : "visible"}
+//       />
+//     </div>
+//   );
+// });
+
+// export default ArtistSongMash;
--- app/src/helpers/random-song.ts
+++ app/src/helpers/random-song.ts
@@ -1,12 +1,10 @@
 export const randomizeSongs = (length: number) => {
-	let indexLeft: number, indexRight: number
-  indexLeft =
-      Math.floor(Math.random() * length) + 1;
-    do {
-      indexRight =
-        Math.floor(Math.random() * length) + 1;
-    } while (indexLeft === indexRight);
+  let indexRight: number;
+  const indexLeft = Math.floor(Math.random() * length) + 1;
+  do {
+    indexRight = Math.floor(Math.random() * length) + 1;
+  } while (indexLeft === indexRight);
 
-	if (indexLeft > indexRight) return [indexRight, indexLeft]
-	return [indexLeft, indexRight]
+  if (indexLeft > indexRight) return [indexRight, indexLeft];
+  return [indexLeft, indexRight];
 };

dec8e722b4f57da101d45b1b7b6046701ba2cc61
Xuan-An Cao <caoxantb@gmail.com>
2023-04-09 14:54:27 +0700

    refactor: code convention

--- server/models/film.ts
+++ server/models/film.ts
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
 
 export interface IFilm extends Document {
-  _id: string,
-	points: number,
-	gallery: string
+  _id: string;
+  points: number;
+  gallery: string;
 }
 
 const filmSchema = new mongoose.Schema({
   _id: String,
   points: Number,
-  gallery: { type: String, ref: "Gallery" }
+  gallery: { type: String, ref: "Gallery" },
 });
 
-const Film = mongoose.model<IFilm>('Film', filmSchema);
+const Film = mongoose.model<IFilm>("Film", filmSchema);
 
 export default Film;

478fc9f65f3603c05d14a4e9279986147b847711
Xuan An Cao <ancx@appurunoMacBook-puro-2.local>
2023-04-23 16:21:58 +0700

    feat: add lang in html head

--- app/src/root.tsx
+++ app/src/root.tsx
@@ -12,7 +12,7 @@ export default component$(() => {
    */
   return (
     <QwikCityProvider>
-      <head>
+      <head lang="en">
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta name="description" content="Put your description here."/>
